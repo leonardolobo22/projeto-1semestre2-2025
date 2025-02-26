7e38735fe1ee555b7325804d4f3febce6dc97d47
leonardolobo22 <leonardolobo@fatec.sp.gov.br>
2025-02-26 19:20:25 -0300

    Atualização Get

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -1,6 +1,9 @@
 package com.fatec.projeto.projeto2025.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,10 @@ public class ExercicioController {
     public String HelloWorld(){
         return "hello"; 
     }
+
+    @GetMapping("{nome}")
+        public String HelloWorld(@PathVariable Optional<String> nome){
+            return nome.isPresent() ? nome.get() : "dd";
+        }
     
 }
