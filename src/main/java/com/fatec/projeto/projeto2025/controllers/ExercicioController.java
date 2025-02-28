f991764f49f8e2bb6e6f364aae438ea863a1ac6a
leonardolobo22 <leonardo.lobo@fatec.sp.gov.br>
2025-02-27 21:15:27 -0300

    Verifica Idade

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 
@@ -12,12 +14,33 @@ public class ExercicioController {
 
     @GetMapping
     public String HelloWorld(){
-        return "hello"; 
+        return "Hello"; 
     }
 
     @GetMapping("{nome}")
         public String HelloWorld(@PathVariable Optional<String> nome){
-            return nome.isPresent() ? nome.get() : "dd";
+             return nome.isPresent() ? nome.get() : "dd:";
         }
     
+    @GetMapping("/get-idade/{idade}")
+    public String RetornaIdade(@PathVariable Integer idade){
+            try {
+                if(idade < 0){
+                    throw new NumberFormatException();
+                }
+
+                if(idade < 12){
+                    return "Criança";
+                }else if (idade <= 18){
+                    return "Adolescente";
+                }else if (idade <= 60){
+                    return "Adulto";
+                }else {
+                    return "Idoso";
+                }
+
+            } catch (Exception e) {
+                return "idade inválida";
+            }
+    }
 }
