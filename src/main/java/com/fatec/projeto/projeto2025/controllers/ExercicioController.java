915c0c8f3903a90a9426ea3ba6466b445af0e5c1
leonardolobo22 <leonardo.lobo@fatec.sp.gov.br>
2025-02-27 22:15:20 -0300

    IMC e verifica par com entrada de carac.

--- src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/ExercicioController.java
@@ -43,4 +43,56 @@ public class ExercicioController {
                 return "idade inválida";
             }
     }
+
+    @GetMapping("/get-num/{num}")
+    public String verificaParImpar(@PathVariable Optional<String> num){
+        num.ifPresent(value -> System.out.println("Numero: " + value));
+ 
+        try{
+        int numInt = Integer.parseInt(num.get());
+        if (numInt % 2 == 0 && numInt > 0){
+            return "Número é par";
+        }else if (numInt % 2 != 0 && numInt > 0){
+            return "Número é impar";
+        }else if (numInt == 0){
+            return "Número é 0";
+        } else {
+            return "Número inválido";
+        }
+    }catch(NumberFormatException e) {
+        return "O valor não é um número, informe novamente";
+    }
+}
+
+@GetMapping("/calc-imc/{peso}/{altura}")
+public String CalcIMC(@PathVariable String peso, @PathVariable String altura){
+    try {
+       
+        double pesoDouble = Double.parseDouble(peso);
+        double alturaDouble = Double.parseDouble(altura);
+     
+        if (pesoDouble <= 0 || alturaDouble <= 0) {
+            return "Dados inválidos";
+        }
+       
+        double imc = pesoDouble / (alturaDouble * alturaDouble);
+       
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            return "Peso normal";
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            return "Sobrepeso";
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            return "Obesidade grau 1";
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            return "Obesidade grau 2";
+        } else {
+            return "Obesidade grau 3";
+        }
+    } catch (NumberFormatException e) {
+        return "Dados inválidos";
+    }
+
+}
 }
